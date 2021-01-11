01e2ae110cb2a52aab623e964b5d24dbbdc9d5b2
Saurabh Mhatre <codeclassifiers@gmail.com>
2021-01-11 11:09:50 +0530

    Questions added

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// Q1. Write code in the below function to print area of sqaure
+const pi = 3.14;
+function areaOfSquare(piInputValue, radiusInputValue) {
+    // Your code here
+
+}
+areaOfSquare(pi, 3);
+
+
+// Q2. Write a function to convert meters to centimeters
+// Ex: If input for the function is 1(in meters) then function should print 100cms
+
+
+
+
+// Q3. Write a function to convert kilometers to meters
+
+
+
+
+// Q4. Write a function to convert minutes to seconds
+// Ex: If input for the function is 1(in minutes) then function should print 60 seconds
